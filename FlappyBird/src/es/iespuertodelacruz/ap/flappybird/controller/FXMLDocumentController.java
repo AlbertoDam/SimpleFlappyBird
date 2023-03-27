d1c0ccffdc4a6cc1fc1ca907227c35a1e5ab5477
MiticoPlayer <aperper.4@gmail.com>
2023-03-27 23:38:14 +0200

    feat: game layout Canva in a Pane

--- FlappyBird/src/es/iespuertodelacruz/ap/flappybird/controller/FXMLDocumentController.java
+++ FlappyBird/src/es/iespuertodelacruz/ap/flappybird/controller/FXMLDocumentController.java
@@ -1,34 +1,32 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXML2.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
  */
 package es.iespuertodelacruz.ap.flappybird.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.layout.Pane;
 
 /**
+ * FXML Controller class
  *
  * @author aperp
  */
 public class FXMLDocumentController implements Initializable {
-    
+
     @FXML
-    private Label label;
-    
+    private Pane paneCont;
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
+    private Canvas canvaPanel;
+
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        
     }    
     
 }
