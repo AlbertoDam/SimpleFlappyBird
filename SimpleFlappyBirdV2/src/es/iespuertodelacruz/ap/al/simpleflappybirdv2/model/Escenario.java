51b398aff28806ef7be3531a6ee525969cc9e267
AngelPRO <angelchiquitopons@gmail.com>
2023-03-30 17:17:38 +0100

    feat(escenario: spawnTube

--- SimpleFlappyBirdV2/src/es/iespuertodelacruz/ap/al/simpleflappybirdv2/model/Escenario.java
+++ SimpleFlappyBirdV2/src/es/iespuertodelacruz/ap/al/simpleflappybirdv2/model/Escenario.java
@@ -4,10 +4,31 @@
  */
 package es.iespuertodelacruz.ap.al.simpleflappybirdv2.model;
 
+import java.util.LinkedList;
+
 /**
  *
  * @author dam
  */
 public class Escenario {
+    
+    private LinkedList<Tuberia> tuberias;
+    private Personaje pajaro;
+    
+    public Escenario(){
+        pajaro = new Personaje();
+        tuberias = new LinkedList();
+    }
+    
+    public void spawnTube(double alto, double ancho){
+        // izq x = ancho; der x = x + 30;
+        // width 30; altura variable con rnd;
+        // izq y = Math.random()*100+(alto/3); der y = y + 30;
+        int width = 30;
+        Tuberia tuberiaSup = new Tuberia(new Punto(ancho,0),new Punto(ancho+width,0),new Punto(ancho, Math.random()*100+(alto/3)),new Punto(ancho+width,Math.random()*100+(alto/3)));
+        //Tuberia tuberiaInf = new Tuberia(new Punto(ancho,alto),new Punto(ancho+width,alto+width),null,null);
+        
+        tuberias.add(null);
+    }
     
 }
