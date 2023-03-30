1553d940b1098555ae80bc6773eb203a1c86d48f
AngelPRO <angelchiquitopons@gmail.com>
2023-03-30 16:39:49 +0100

    feat: getter, setter y constr de punto y tuberia

--- SimpleFlappyBirdV2/src/es/iespuertodelacruz/ap/al/simpleflappybirdv2/model/Punto.java
+++ SimpleFlappyBirdV2/src/es/iespuertodelacruz/ap/al/simpleflappybirdv2/model/Punto.java
@@ -10,4 +10,25 @@ package es.iespuertodelacruz.ap.al.simpleflappybirdv2.model;
  */
 public class Punto {
     
+    private double x;
+    private double y;
+
+    public Punto(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+    
+    public double getY() {
+        return y;
+    }
+      
+    public void sumar(Punto punto){
+        this.x += punto.getX();
+        this.y += punto.getY();
+    }
+    
 }
--- SimpleFlappyBirdV2/src/es/iespuertodelacruz/ap/al/simpleflappybirdv2/model/Tuberia.java
+++ SimpleFlappyBirdV2/src/es/iespuertodelacruz/ap/al/simpleflappybirdv2/model/Tuberia.java
@@ -10,4 +10,56 @@ package es.iespuertodelacruz.ap.al.simpleflappybirdv2.model;
  */
 public class Tuberia {
     
+    private Punto limInfIzq;
+    private Punto limInfDer;
+    private Punto limSupDer;
+    private Punto limSupIzq;
+
+    public Tuberia(Punto limInfIzq, Punto limInfDer, Punto limSupDer, Punto limSupIzq) {
+        this.limInfIzq = limInfIzq;
+        this.limInfDer = limInfDer;
+        this.limSupDer = limSupDer;
+        this.limSupIzq = limSupIzq;
+    }
+    
+    public Punto getLimInfIzq() {
+        return limInfIzq;
+    }
+
+    public void setLimInfIzq(Punto limInfIzq) {
+        this.limInfIzq = limInfIzq;
+    }
+
+    public Punto getLimInfDer() {
+        return limInfDer;
+    }
+
+    public void setLimInfDer(Punto limInfDer) {
+        this.limInfDer = limInfDer;
+    }
+
+    public Punto getLimSupDer() {
+        return limSupDer;
+    }
+
+    public void setLimSupDer(Punto limSupDer) {
+        this.limSupDer = limSupDer;
+    }
+
+    public Punto getLimSupIzq() {
+        return limSupIzq;
+    }
+
+    public void setLimSupIzq(Punto limSupIzq) {
+        this.limSupIzq = limSupIzq;
+    }
+    
+    public void res(double valor){
+        Punto aux = new Punto((-1*valor),0);
+        this.limInfIzq.sumar(aux);
+        this.limInfDer.sumar(aux);
+        this.limSupDer.sumar(aux);
+        this.limSupIzq.sumar(aux);
+    }
+    
 }
